bcd4afd2f9393059cf76bd095b325bfcf4c0f4fc
bhandariamey <bhandariamey@gmail.com>
2023-10-30 12:37:52 +0530

    TDS front page complete

--- index.js
+++ index.js
@@ -1,3 +1,91 @@
 
 console.log("code in js")
-user_review_div = document.getElementById("review-header-main-child")
+const client_details = [
+    {
+        project_name : "Thunderstorm",
+        project_style: "Art Modern",
+        main_image : "./photos/sites/thunderstorm.jpg",
+        client_name : "Mr. Jain",
+        date : 2020,
+        description: "The Dry Creek Building brings together key business partners in a common location and is designed to help drive collaboration and innovation, ultimately helping to move Gallo’s business forward. The new office building complements existing structures and is a central location between the campus’s multiple buildings, making is a vital hub for employees to gather, socialize, and collaborate.",
+        carousel_images:{
+
+        }
+
+
+    },
+    {
+        project_name : "Aapli",
+        project_style: "Minimalist",
+        main_image : "./photos/sites/aapli.jpg",
+        client_name : "Mr. Sheth",
+        date : "Sept. 2023",
+        description: "The Dry Creek Building brings together key business partners in a common location and is designed to help drive collaboration and innovation, ultimately helping to move Gallo’s business forward. The new office building complements existing structures and is a central location between the campus’s multiple buildings, making is a vital hub for employees to gather, socialize, and collaborate.",
+        carousel_images:{
+            
+        }
+    },
+
+    {
+        project_name : "BlueBird",
+        project_style: "Light House",
+        main_image : "./photos/sites/BlueBird.jpeg",
+        client_name : "Mr. Desarda",
+        date : "Sept. 2023",
+        description: "The Dry Creek Building brings together key business partners in a common location and is designed to help drive collaboration and innovation, ultimately helping to move Gallo’s business forward. The new office building complements existing structures and is a central location between the campus’s multiple buildings, making is a vital hub for employees to gather, socialize, and collaborate.",
+        carousel_images:{
+            
+        }
+    },
+
+    {
+        project_name : "Mantra Monarch",
+        project_style: "Home",
+        main_image : "./photos/sites/MantraMonarch.jpg",
+        client_name : "Mr. Ghotkar",
+        date : "Nov. 2023",
+        description: "The Dry Creek Building brings together key business partners in a common location and is designed to help drive collaboration and innovation, ultimately helping to move Gallo’s business forward. The new office building complements existing structures and is a central location between the campus’s multiple buildings, making is a vital hub for employees to gather, socialize, and collaborate.",
+        carousel_images:{
+            
+        }
+    }
+
+
+]
+
+// // <div class="col-md-6 col-sm-12">
+
+// <a href="#"><img src="./photos/sites/thunderstorm.jpg" width="100%" alt="site1"></a>
+// <div class="row" style="margin-top: 20px;">
+//   <div class="col">
+//     <a href="#"><h2 class="site-desc">Fit Haven Thunderstorm</h2></a>
+//     <a href="#"><p class="site-style">Art Modern</p></a>
+//   </div>
+//   <div class="col-md-6 col-sm-12 arrow-right-div"><a href="#"><button class="arrow-right"></button></a></div>
+// </div>
+
+// </div>
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var sites_div = document.querySelector(".sites"); 
+    client_details.forEach((site) => {
+        var div = document.createElement("div");
+        div.setAttribute("class", "col-md-6 col-sm-12");
+        div.innerHTML =
+         `
+            <a href="#"><img src="${site.main_image}" width="100%" alt="${site.project_name}"></a>
+            <div class="row" style="margin-top: 20px;">
+                <div class="col">
+                    <a href="#"><h2 class="site-desc">${site.project_name}</h2></a>
+                    <a href="#"><p class="site-style">${site.project_style}</p></a>
+                </div>
+                <div class="col-md-6 col-sm-12 arrow-right-div">
+                    <a href="#"><button class="arrow-right"></button></a>
+                </div>
+            </div>
+        `
+        sites_div.appendChild(div);
+    });
+});
+
