f57fadb094a7b731f6f99eedefe203c249f98479
bhandariamey <bhandariamey@gmail.com>
2023-11-23 20:56:05 +0530

    Service page Step-1 completed

--- contact.js
+++ contact.js
@@ -1,18 +1,26 @@
-console.log("Inside contacts js")
-document.addEventListener("DOMContentLoaded",()=>{
+console.log("Inside contacts js");
+
+document.addEventListener("DOMContentLoaded", () => {
 
     var scroll_top = document.querySelector(".scroll-to-top");
-    scroll_top.addEventListener("click",(e)=>{
+    scroll_top.addEventListener("click", (e) => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
-        })
-    })
+        });
+    });
 
-    var contact_form = document.getElementById("user-details");
-   // console.log("working till here")
+    // var contact_form = document.getElementById("user-details");
 
-    contact_form.addEventListener("submit",(e)=>{
-   // contact_form.reset();
-    })
-})
+    // contact_form.addEventListener("submit", async (e) => {
+    //     e.preventDefault(); 
+    //     const result = await getAPIResponse();
+    //     console.log("Printing response", result);
+    // });
+});
+
+// async function getAPIResponse() {
+//     console.log("getAPIResponse function called");
+//     const response = await axios.get("https://formspree.io/f/xdorvbgq");
+//     return response;
+// }
