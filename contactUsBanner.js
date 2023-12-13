f88788dc605a609c021cd64357794e916165a22b
Amey Bhandari <bhandariamey@gmail.com>
2023-12-13 15:19:53 +0530

    Adding some real content

--- contactUsBanner.js
+++ contactUsBanner.js
@@ -7,7 +7,7 @@ contactUSBanner.innerHTML =
 <p>(+91) 90750-40505</p>
 <div class="d-flex flex-row justify-content-center">
   <div class="col4"> <a target=_blank href="https://www.instagram.com/thedesignspell/"><img src="./photos/instagram.png" alt="Instragram icon" width="30px" height="30px"></a></div>
-  <div class="col4"><a target=_blank href="https://www.linkedin.com/in/sanjana-dugad-5330a2144/"><img style="width:30px; height: 30px; margin: 0px 30px;"  src="./photos/linkedIn.png" alt="LinkedIn icon" width="50px" height="50px"></a></div>
+  <div class="col4"><a target=_blank href="https://www.linkedin.com/in/sanjana-dugad-5330a2144/"><img src="./photos/linkedIn.png style="width:30px; height: 30px; margin: 0px 30px; alt="LinkedIn icon" width="50px" height="50px"></a></div>
   <div class="col4"><a target=_blank href="https://wa.me/message/VPMG4ROHK5IMD1"><img src="./photos/whatsapp.png" alt="Instragram icon" width="30px" height="30px"></a></div>
 </div>
 </div>
