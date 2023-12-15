c1a74b9478eedb1c486973efb757a428f59d5650
Amey Bhandari <bhandariamey@gmail.com>
2023-12-15 12:07:48 +0530

    ContactUs banner added for site.html page

--- contactUsBanner.js
+++ contactUsBanner.js
@@ -5,10 +5,22 @@ contactUSBanner.innerHTML =
 <p>Flat no. 104 : Vrundavan Apartments, Near Golden Wheel Hotel , </br>Indian Oil Petrol Pump Lane, Navi Peth, Pune, Maharashtra 411030</p>
 <p>sanjana@thedesignspell.com</p>
 <p>(+91) 90750-40505</p>
-<div class="d-flex flex-row justify-content-center">
-  <div class="col4"> <a target=_blank href="https://www.instagram.com/thedesignspell/"><img src="./photos/instagram.png" alt="Instragram icon" width="30px" height="30px"></a></div>
-  <div class="col4"><a target=_blank href="https://www.linkedin.com/in/sanjana-dugad-5330a2144/"><img src="./photos/linkedin.png"; style="width:30px; height: 30px; margin: 0px 30px;" alt="LinkedIn icon" width="50px" height="50px"></a></div>
-  <div class="col4"><a target=_blank href="https://wa.me/message/VPMG4ROHK5IMD1"><img src="./photos/whatsapp.png" alt="Instragram icon" width="30px" height="30px"></a></div>
+<div class="d-flex flex-row justify-content-center text-center mt-4">
+  <div class="col-1 d-inline-block">
+    <a target='_blank' href="https://www.instagram.com/thedesignspell/">
+      <img src="./photos/instagram.png" alt="Instragram icon" width="30px" height="30px">
+    </a>
+  </div>
+  <div class="col-1 d-inline-block">
+    <a target='_blank' href="https://www.linkedin.com/in/sanjana-dugad-5330a2144/">
+      <img src="./photos/linkedin.png" alt="LinkedIn icon" width="30px" height="30px">
+    </a>
+  </div>
+  <div class="col-1 d-inline-block">
+    <a target='_blank' href="https://wa.me/message/VPMG4ROHK5IMD1">
+      <img src="./photos/whatsapp.png" alt="WhatsApp icon" width="30px" height="30px">
+    </a>
+  </div>
 </div>
-</div>
+
 `
